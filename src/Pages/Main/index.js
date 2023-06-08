77c6d5dc24ab19976c480423d770fe178e95b5da
KireMitrov <kire_val@yahoo.com>
2023-06-08 14:02:15 +0200

    Rounded the numbers

--- src/Pages/Main/index.js
+++ src/Pages/Main/index.js
@@ -5,31 +5,31 @@ import bg from "../../assets/bg.jpg"
 
 
 function Main() {
-    
+
     const [lat, setLat] = useState('');
     const [long, setLong] = useState('');
     const [data, setData] = useState({});
     const [city, setCity] = useState('');
-    
-    
+
+
     useEffect(() => {
-        
-        navigator.geolocation.getCurrentPosition(function(position) {
+
+        navigator.geolocation.getCurrentPosition(function (position) {
             setLat(position.coords.latitude);
             setLong(position.coords.longitude);
         })
         console.log(long)
-        
+
     }, [])
-    
+
 
     useEffect(() => {
         const apiCurrent = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${process.env.REACT_APP_API_KEY}&units=metric`
         axios.get(apiCurrent).then((response) => {
             setData(response.data)
         })
-        
-    },[long, lat])
+
+    }, [long, lat])
 
     const searchLocation = () => {
         const apiBySearch = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${process.env.REACT_APP_API_KEY}&units=metric`
@@ -37,18 +37,18 @@ function Main() {
             setData(response.data)
         })
     }
-    
+
     const background = {
         backgroundImage: `url(${bg})`,
         backgroundSize: "cover",
         backgroundPosition: 'top center',
         height: "100vh",
     }
-    
-    return(
-        
-        <div style={background}>
-        <div className="search-container">
+
+    return (
+
+        <div style={background} className="main-container">
+            <div className="search-container">
                 <input className="search" type="text" placeholder="Search for a city..." value={city} onChange={(e) => setCity(e.target.value)}></input>
                 <button className="search-btn" onClick={searchLocation}>Search</button>
             </div>
@@ -56,11 +56,11 @@ function Main() {
                 <div className="city">{data.name}</div>
             </div>
             <div className="current-weather">
-                <div className="temperature">{data.main && data.main.temp} &deg;C</div>
+                <div className="temperature">{data.main && Math.round(data.main.temp)} &deg;C</div>
                 <div className="weather">{data.weather && data.weather[0].main}
-                <img src={`http://openweathermap.org/img/w/${data.weather && data.weather[0].icon}.png`} alt="current weather" />
+                    <img src={`http://openweathermap.org/img/w/${data.weather && data.weather[0].icon}.png`} alt="current weather" />
                 </div>
-                <div className="hi-low">Hi: {data.main && data.main.temp_min} &deg;C Low: {data.main && data.main.temp_max} &deg;C</div>
+                <div className="hi-low">Hi: {data.main && Math.round(data.main.temp_min)} &deg;C Low: {data.main && Math.round(data.main.temp_max)} &deg;C</div>
                 <div className="humidity">Humidity: {data.main && data.main.humidity} %</div>
             </div>
         </div>
